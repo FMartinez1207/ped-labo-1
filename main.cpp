43bd01a0deb69dd00b3e97f36716e7e821986f5a
”josueosorio13826” <00013826@uca.edu.sv>
2026-09-10 17:43:06 -0600

    feat: funcion depositar por referencia

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void mostrarBalance(float balance);
+void depositar(float &balance);
+
+int main() {
+    float balance = 0;
+    int op = 0;
+
+    while (op != 4) {
+        cout << "\n--- MENU CAJERO ---" << endl;
+        cout << "1. Mostrar balance\n2. Depositar\n3. Retirar\n4. Salir\nOpcion: ";
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                mostrarBalance(balance);
+                break;
+
+                case 2: depositar(balance);
+                  break;
+            case 4:
+                cout << "\nSaliendo del sistema..." << endl;
+                break;
+            default:
+                cout << "Opcion invalida, intente de nuevo." << endl;
+        }
+    }
+
+    return 0;
+}
